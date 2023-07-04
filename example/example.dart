91735bd7458ceca1801da7f90d1fc02fd2a71231
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-07-04 10:42:45 +0800

    add example

--- example/example.dart
+++ example/example.dart
@@ -1,1 +1,14 @@
+import 'dart:io';
 
+import 'package:rhino_script/src/lexer.dart';
+import 'package:rhino_script/src/token.dart';
+
+void main(List<String> args) {
+  final String sourceCode = File('example/sample.rhs').readAsStringSync();
+
+  final List<Token> tokens = Lexer.tokenize(sourceCode);
+
+  print('source code:\n$sourceCode');
+  print('tokens:');
+  tokens.forEach(print);
+}
