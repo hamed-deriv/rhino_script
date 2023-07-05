68543ce73505053ed296c36a8f7ec67e8a893b65
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-07-05 15:35:00 +0800

    minor refactor scope class

--- lib/src/scope.dart
+++ lib/src/scope.dart
@@ -7,29 +7,34 @@ class Scope {
   final Map<String, RuntimeValue> _values = <String, RuntimeValue>{};
   final Set<String> _constants = <String>{};
 
+  RuntimeValue get(String name) => _resolve(name)._values[name]!;
+
   RuntimeValue define(String name, RuntimeValue value, bool isConstant) {
-    if (_values.containsKey(name)) {
+    if (_isDefined(name)) {
       throw Exception('Variable ($name) is already defined.');
     }
 
-    if (isConstant) {
-      _constants.add(name);
-    }
+    _setConstant(name);
 
-    return _values[name] = value;
+    return _set(name, value);
   }
 
   RuntimeValue assign(String name, RuntimeValue value) {
-    final Scope scope = _resolve(name);
-
-    if (scope._constants.contains(name)) {
+    if (_isConstant(name)) {
       throw Exception('Cannot reassign constant value ($name).');
     }
 
-    return scope._values[name] = value;
+    return _set(name, value);
   }
 
-  RuntimeValue get(String name) => _resolve(name)._values[name]!;
+  RuntimeValue _set(String name, RuntimeValue value) =>
+      _resolve(name)._values[name] = value;
+
+  void _setConstant(String name) => _resolve(name)._constants.add(name);
+
+  bool _isDefined(String name) => _resolve(name)._values.containsKey(name);
+
+  bool _isConstant(String name) => _resolve(name)._constants.contains(name);
 
   Scope _resolve(String name) {
     if (_values.containsKey(name)) {
